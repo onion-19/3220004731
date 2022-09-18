fc92204cbd8f084ee6d340ece0d6bbdc7912d21b
onion <1776920394@qq.com>
2022-09-18 16:35:41 +0800

    从命令行获取文件路径

--- 3220004731/src/main/java/edu/papers/entry/Entry.java
+++ 3220004731/src/main/java/edu/papers/entry/Entry.java
@@ -1,6 +1,18 @@
 package edu.papers.entry;
 
+import com.huaban.analysis.jieba.SegToken;
+import edu.papers.utils.Comparator;
+import edu.papers.utils.FileUtil;
+import edu.papers.utils.Segmenter;
+
+import java.util.List;
+
 public class Entry {
     public static void main(String[] args) {
+        List<SegToken> orig = Segmenter.seg(FileUtil.readFile(args[0]));
+        List<SegToken> test = Segmenter.seg(FileUtil.readFile(args[1]));
+
+        FileUtil.writeFile(args[0], args[1], Comparator.compare(orig, test), args[2]);
+
     }
 }
